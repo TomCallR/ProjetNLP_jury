40fb247d3717ba2f4b98dcc2a44f1c249873aac2
TomCallR <thomas.calladine@gmail.com>
2019-11-10 11:47:34 +0100

    Table creation through ORM (updated)

--- app/orm.py
+++ app/orm.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
-# One course to many students and to many sheets
+# One course to many students and to many forms (one form per week at most)
 class Course(Base):
     __tablename__ = 'Courses'
 
@@ -18,7 +18,7 @@ class Course(Base):
 
     students = relationship('Student', back_populates='course')
     questions = relationship('Question', back_populates='course')
-    sheets = relationship('Sheet', back_populates='course')
+    forms = relationship('Form', back_populates='course')
 
     def __repr__(self):
         return '<Course {} {}>'.format(self.id, self.label)
@@ -41,28 +41,34 @@ class Student(Base):
         return '<Student {} {} {}>'.format(self.id, self.firstname, self.lastname)
 
 
-# One sheet collects the answers for one week from the students in one course
-# One sheet is in a many to one relationship with a Course
-class Sheet(Base):
-    __tablename__ = 'Sheets'
+# One form corresponds to one sheet in a spreadsheet
+# It collects the answers for one week from the students in one course
+# One form is in a many to one relationship with a Course
+class Form(Base):
+    __tablename__ = 'Forms'
 
     id = Column(Integer, primary_key=True)
     sheetid = Column(String(80), nullable=False)
-    label = Column(String(80))
+    sheetlabel = Column(String(80))
+    lastentrydate = Column(Date)
+    lastreaddate = Column(Date)
     course_id = Column(Integer, ForeignKey('Courses.id'))
 
-    course = relationship('Course', back_populates='sheets')
-    answers = relationship('Answer', back_populates='sheet')
+    course = relationship('Course', back_populates='forms')
+    answers = relationship('Answer', back_populates='form')
 
     def __repr__(self):
         return '<Sheet {} {}>'.format(self.id, self.label)
 
 
 # Questions for each course (many to one)
+# Question type is in fact answer type : grade (1 to 5), choice (non numeric), text
+# Question type is inferred by the app
 class Question(Base):
     __tablename__ = 'Questions'
 
     id = Column(Integer, primary_key=True)
+    type = Column(Integer)
     text = Column(String(200), unique=True, nullable=False)
     course_id = Column(Integer, ForeignKey('Courses.id'))
 
@@ -73,7 +79,7 @@ class Question(Base):
         return '<Question {} {}>'.format(self.id, self.text)
 
 
-# Answer : one answer per student per sheet (=> per week) per question
+# Answer : one answer per student per form (=> per week) per question
 class Answer(Base):
     __tablename__ = 'Answers'
 
@@ -82,11 +88,11 @@ class Answer(Base):
     text = Column(String(1000))
     student_id = Column(Integer, ForeignKey('Students.id'))
     question_id = Column(Integer, ForeignKey('Questions.id'))
-    sheet_id = Column(Integer, ForeignKey('Sheets.id'))
+    form_id = Column(Integer, ForeignKey('Forms.id'))
 
     student = relationship('Student', back_populates='answers')
     question = relationship('Question', back_populates='answers')
-    sheet = relationship('Sheet', back_populates='answers')
+    form = relationship('Form', back_populates='answers')
 
     def __repr__(self):
         return '<Answer {} {} {}>'.format(self.id, self.horodateur, self.reponse)
